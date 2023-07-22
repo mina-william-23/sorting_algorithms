582221b7e90b2f708cf10d53e923e2d1fea3b4d2
mina-william-23 <capeandroid50@gmail.com>
2023-07-22 15:15:54 -0700

    edit style

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,43 +12,43 @@
  */
 void quick_sort_lumuto(int *array, size_t start, size_t end, size_t sz)
 {
-size_t i, left;
-int temp, pivot;
+	size_t i, left;
+	int temp, pivot;
 
-/* if size less than or equal to 1 don't need sort */
-/* +1 to compute length because end, start is indexes */
-/* this also resolve the over flow as size_t if (left)end = 0 */
-/* and left -1 = will give overflow but +1 resolve that issue */
-/* if it was just signed int condition (end <= start) would be enough */
-if (end - start + 1 <= 1)
-return;
+	/* if size less than or equal to 1 don't need sort */
+	/* +1 to compute length because end, start is indexes */
+	/* this also resolve the over flow as size_t if (left)end = 0 */
+	/* and left -1 = will give overflow but +1 resolve that issue */
+	/* if it was just signed int condition (end <= start) would be enough */
+	if (end - start + 1 <= 1)
+		return;
 
-pivot = array[end];
-left = start;
-for (i = start; i < end; i++)
-{
-if (array[i] < pivot)
-{
-if (left != i)
-{
-temp = array[left];
-array[left] = array[i];
-array[i] = temp;
-print_array(array, sz);
-}
-left++;
-}
-}
-/* left is right place for pivot so swap left with pivot(end) */
-if (array[left] != array[end])
-{
-array[end] = array[left];
-array[left] = pivot;
-print_array(array, sz);
-}
+	pivot = array[end];
+	left = start;
+	for (i = start; i < end; i++)
+	{
+		if (array[i] < pivot)
+		{
+			if (left != i)
+			{
+				temp = array[left];
+				array[left] = array[i];
+				array[i] = temp;
+				print_array(array, sz);
+			}
+			left++;
+		}
+	}
+	/* left is right place for pivot so swap left with pivot(end) */
+	if (array[left] != array[end])
+	{
+		array[end] = array[left];
+		array[left] = pivot;
+		print_array(array, sz);
+	}
 
-quick_sort_lumuto(array, start, left - 1, sz);
-quick_sort_lumuto(array, left + 1, end, sz);
+	quick_sort_lumuto(array, start, left - 1, sz);
+	quick_sort_lumuto(array, left + 1, end, sz);
 }
 
 /**
@@ -58,7 +58,7 @@ quick_sort_lumuto(array, left + 1, end, sz);
  */
 void quick_sort(int *array, size_t size)
 {
-if (!array || size <= 1)
-return;
-quick_sort_lumuto(array, 0, size - 1, size);
+	if (!array || size <= 1)
+		return;
+	quick_sort_lumuto(array, 0, size - 1, size);
 }
