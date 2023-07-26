eb662e5f6b2de5350ecbcd7dcdcdf137a40e4248
mina-william-23 <capeandroid50@gmail.com>
2023-07-25 19:05:19 -0700

    edit count sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -3,18 +3,15 @@
 #include "sort.h"
 
 /**
- * counting_sort - count sort algorithm
- * @array: array
- * @size: size
- */
-void counting_sort(int *array, size_t size)
+* get_max - get max int from array
+* @array: arary
+* @size: array size
+* Return: max int in array
+*/
+int get_max(int *array, size_t size)
 {
-	int *cnt_arr, mx;
-	size_t idx, cnt_idx;
-	char *delim = "";
-
-	if (!array || size < 2)
-		return;
+	int mx;
+	size_t idx;
 
 	mx = array[0];
 	for (idx = 1; idx < size; idx++)
@@ -22,31 +19,54 @@ void counting_sort(int *array, size_t size)
 		if (array[idx] > mx)
 			mx = array[idx];
 	}
-	cnt_arr = malloc(sizeof(int) * (mx + 1));
-	if (!cnt_arr)
+	return (mx);
+}
+
+/**
+ * counting_sort - count sort algorithm
+ * @array: array
+ * @size: size
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *b, *cnt_arr, mx;
+	size_t idx, cnt_idx;
+
+	if (!a || size < 2)
 		return;
-	for (cnt_idx = 0; cnt_idx <= (size_t)mx; cnt_idx++)
-		cnt_arr[cnt_idx] = 0;
+
+	b = malloc(sizeof(int) * size);
+	if (!b)
+		return;
 	for (idx = 0; idx < size; idx++)
-		cnt_arr[array[idx]]++;
-	for (cnt_idx = 0, idx = 0; cnt_idx <= (size_t)mx; cnt_idx++)
+		b[idx] = a[idx];
+
+	mx = get_max(a, size);
+
+	cnt_arr = malloc(sizeof(int) * mx + 1);
+	if (!cnt_arr)
 	{
-		if (cnt_arr[cnt_idx])
-		{
-			/* replace the old array with new data sorted */
-			while (cnt_arr[cnt_idx]--)
-			{
-				array[idx++] = cnt_idx;
-				printf("%s%lu", delim, idx);
-				delim = ", ";
-			}
-		}
-		else
-		{
-			printf("%s%lu", delim, idx);
-		}
-		delim = ", ";
+		free(b);
+		return;
 	}
-	printf("\n");
+	for (cnt_idx = 0; cnt_idx <= mx; cnt_idx++)
+		cnt_arr[cnt_idx] = 0;
+
+	for (idx = 0; idx < size; idx++)
+		cnt_arr[a[idx]]++;
+
+	for (cnt_idx = 1; cnt_idx <= (size_t) mx; cnt_idx++)
+		cnt_arr[cnt_idx] += cnt_arr[cnt_idx - 1];
+	print_array(cnt_arr, mx + 1);
+
+	for (idx = size - 1; idx >= 0; idx--)
+	{
+		b[--cnt_arr[a[idx]]] = a[idx];
+		if (idx == 0)
+			break;
+	}
+	for (idx = 0; idx < size; idx++)
+		a[idx] = b[idx];
+
 	free(cnt_arr);
 }
